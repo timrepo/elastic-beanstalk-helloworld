66a5222a21253f9d12fd39b13ee3c496b597f430
timrepo <timli.mv@gmail.com>
2023-11-30 17:05:11 -0800

    init checkin

---
+++ README.md
@@ -0,0 +1,4 @@
+# hello world app deployed to Elastic Beanstalk
+
+## another sample app for go+gin
+https://github.com/sudo-suhas/go-beanstalk-gin
---
+++ sample_eb_cmd.sh
@@ -0,0 +1,1 @@
+eb create timdocker-compose-test --enable-spot --region us-east-1 --instance_type t2.micro --instance_profile amira-elasticbeanstalk-ec2-role-2
